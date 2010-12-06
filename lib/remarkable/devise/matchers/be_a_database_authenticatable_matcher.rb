f7e3e889661201fe0230ffd4a3566834339375bf
Vasily Reys <reys.vasily@gmail.com>
2010-12-06 19:51:44 +0600

    Matcher be_a_database_authenticatable is support options (:stretches and :encryptor) from now

--- lib/remarkable/devise/matchers/be_a_database_authenticatable_matcher.rb
+++ lib/remarkable/devise/matchers/be_a_database_authenticatable_matcher.rb
@@ -2,17 +2,33 @@ module Remarkable
   module Devise
     module Matchers
       class BeADatabaseAuthenticatableMatcher < Base
-        assertion :included?, :has_email_column?, :has_encrypted_password_column?, :has_password_salt_column?
+        arguments
+        assertion :included?, :has_email_column?, :has_encrypted_password_column?, :has_password_salt_column?,
+        :stretches_match?, :encryptor_match?
+
+        optionals :stretches, :encryptor
 
         protected
         
         def included?
           subject_class.ancestors.include?(::Devise::Models::DatabaseAuthenticatable)
         end
+
+        def stretches_match?
+          return true unless @options.key?(:stretches)
+
+          return subject_class.stretches == @options[:stretches], :actual => subject_class.stretches.inspect
+        end
+
+        def encryptor_match?
+          return true unless @options.key?(:encryptor)
+
+          return subject_class.encryptor == @options[:encryptor], :actual => subject_class.encryptor.inspect
+        end
       end
       
-      def be_a_database_authenticatable
-        BeADatabaseAuthenticatableMatcher.new
+      def be_a_database_authenticatable(*args, &block)
+        BeADatabaseAuthenticatableMatcher.new(*args, &block).spec(self)
       end
     end
   end
--- spec/matchers/be_a_database_authenticatable_spec.rb
+++ spec/matchers/be_a_database_authenticatable_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
+class Bar < ActiveRecord::Base
+  devise :database_authenticatable, :stretches => 15, :encryptor => :clearance_sha1
+end
+
 describe Remarkable::Devise::Matchers::BeADatabaseAuthenticatableMatcher do
   before do
     @valid_columns = ['email', 'encrypted_password', 'password_salt']
 
     User.stubs(:column_names).returns(@valid_columns)
+    Bar.stubs(:column_names).returns(@valid_columns)
   end
 
   context "inclusion of Devise::Models::DatabaseAuthenticatable" do
@@ -13,7 +18,29 @@ describe Remarkable::Devise::Matchers::BeADatabaseAuthenticatableMatcher do
     end
 
     it "should validate that model has no :database_authenticatable model" do
-       subject.matches?(FooUser).should be_false
+      subject.matches?(FooUser).should be_false
+    end
+  end
+
+  context "options validation" do
+    describe :stretches do
+      it "should validate that a model has proper :stratches" do
+        subject.class.new(:stretches => 15).matches?(Bar).should be_true
+      end
+
+      it "should validate that a model hasn't proper :stratches" do
+        subject.class.new(:stretches => 10).matches?(Bar).should be_false
+      end
+    end
+
+    describe :encryptor do
+      it "should validate that a model has proper :encryptor" do
+        subject.class.new(:encryptor => :clearance_sha1).matches?(Bar).should be_true
+      end
+
+      it "should validate that a model hasn't proper :encryptor" do
+        subject.class.new(:encryptor => :bcrypt).matches?(Bar).should be_false
+      end
     end
   end
   
@@ -120,6 +147,24 @@ describe Remarkable::Devise::Matchers::BeADatabaseAuthenticatableMatcher do
       end
 
       specify { @msg.should match('to have password_salt column') }
+    end
+
+    context "when :stretches doesn't match" do
+      before do
+        @matcher = subject.class.new(:stretches => 10)
+        @matcher.matches?(Bar)
+      end
+
+      specify { @matcher.failure_message_for_should.should match('Bar to have password stretches equal to 10, got 15') }
+    end
+
+    context "when :encryptor doesn't match" do
+      before do
+        @matcher = subject.class.new(:encryptor => :bcrypt)
+        @matcher.matches?(Bar)
+      end
+
+      specify { @matcher.failure_message_for_should.should match('Bar to have :bcrypt password encryptor, got :clearance_sha1') }
     end
   end
 end
